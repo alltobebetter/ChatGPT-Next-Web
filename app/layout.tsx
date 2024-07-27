5ce4628a27f95364efe6a404a68e2bd4681c6bb0
苏淋 <108602313+alltobebetter@users.noreply.github.com>
2024-07-27 20:58:19 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "苏淋の助手",
+  description: "您的多模态的AI智能助手🍨",
   appleWebApp: {
-    title: "NextChat",
+    title: "苏淋の助手",
     statusBarStyle: "default",
   },
 };
