d66c778c0891c81da0a61317e72eb4317891235b
苏淋 <108602313+alltobebetter@users.noreply.github.com>
2024-10-20 08:50:34 +0800

    Update mask.tsx

--- app/components/mask.tsx
+++ app/components/mask.tsx
@@ -157,7 +157,7 @@ export function MaskConfig(props: {
           <input
             aria-label={Locale.Mask.Config.HideContext.Title}
             type="checkbox"
-            checked={props.mask.hideContext}
+            checked={props.mask.hideContext ?? true}
             onChange={(e) => {
               props.updateMask((mask) => {
                 mask.hideContext = e.currentTarget.checked;
