c923d912efa2ad6e5f876687a155afacfed0b226
苏淋 <108602313+alltobebetter@users.noreply.github.com>
2024-10-22 18:04:14 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "AIBoB | 下一代AI交互",
+  title: "AIBoB - 下一代AI交互",
   description: "编程 / 画图 / 视频 / 识图 / 文件 🍩",
   appleWebApp: {
     title: "aibob",
