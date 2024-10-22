f5360f4fa48a51ac76759586940b86d6745cee04
苏淋 <108602313+alltobebetter@users.noreply.github.com>
2024-10-22 08:46:00 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -8,6 +8,7 @@ declare global {
 
       OPENAI_API_KEY?: string;
       CODE?: string;
+      PUBLIC_MODELS?: string;
 
       BASE_URL?: string;
       OPENAI_ORG_ID?: string; // openai only
@@ -107,128 +108,69 @@ function getApiKey(keys?: string) {
 }
 
 export const getServerSideConfig = () => {
-  if (typeof process === "undefined") {
-    throw Error(
-      "[Server Config] you are importing a nodejs-only module outside of nodejs",
-    );
-  }
+  const apiKey = getApiKey(process.env.OPENAI_API_KEY);
 
-  const disableGPT4 = !!process.env.DISABLE_GPT4;
-  let customModels = process.env.CUSTOM_MODELS ?? "";
-  let defaultModel = process.env.DEFAULT_MODEL ?? "";
-
-  if (disableGPT4) {
-    if (customModels) customModels += ",";
-    customModels += DEFAULT_MODELS.filter(
-      (m) =>
-        (m.name.startsWith("gpt-4") || m.name.startsWith("chatgpt-4o")) &&
-        !m.name.startsWith("gpt-4o-mini"),
-    )
-      .map((m) => "-" + m.name)
-      .join(",");
-    if (
-      (defaultModel.startsWith("gpt-4") ||
-        defaultModel.startsWith("chatgpt-4o")) &&
-      !defaultModel.startsWith("gpt-4o-mini")
-    )
-      defaultModel = "";
-  }
-
-  const isStability = !!process.env.STABILITY_API_KEY;
-
-  const isAzure = !!process.env.AZURE_URL;
-  const isGoogle = !!process.env.GOOGLE_API_KEY;
-  const isAnthropic = !!process.env.ANTHROPIC_API_KEY;
-  const isTencent = !!process.env.TENCENT_API_KEY;
-
-  const isBaidu = !!process.env.BAIDU_API_KEY;
-  const isBytedance = !!process.env.BYTEDANCE_API_KEY;
-  const isAlibaba = !!process.env.ALIBABA_API_KEY;
-  const isMoonshot = !!process.env.MOONSHOT_API_KEY;
-  const isIflytek = !!process.env.IFLYTEK_API_KEY;
-  // const apiKeyEnvVar = process.env.OPENAI_API_KEY ?? "";
-  // const apiKeys = apiKeyEnvVar.split(",").map((v) => v.trim());
-  // const randomIndex = Math.floor(Math.random() * apiKeys.length);
-  // const apiKey = apiKeys[randomIndex];
-  // console.log(
-  //   `[Server Config] using ${randomIndex + 1} of ${apiKeys.length} api key`,
-  // );
-
-  const allowedWebDavEndpoints = (
-    process.env.WHITE_WEBDAV_ENDPOINTS ?? ""
-  ).split(",");
+  const publicModels = (process.env.PUBLIC_MODELS ?? "").split(",").filter(v => v.trim().length > 0);
 
   return {
+    apiKey,
+    code: ACCESS_CODES,
+    codes: ACCESS_CODES,
+    needCode: ACCESS_CODES.size > 0,
     baseUrl: process.env.BASE_URL,
-    apiKey: getApiKey(process.env.OPENAI_API_KEY),
     openaiOrgId: process.env.OPENAI_ORG_ID,
+    isVercel: !!process.env.VERCEL,
+    hideUserApiKey: !!process.env.HIDE_USER_API_KEY,
+    disableGPT4: !!process.env.DISABLE_GPT4,
+    hideBalanceQuery: !process.env.ENABLE_BALANCE_QUERY,
+    disableFastLink: !!process.env.DISABLE_FAST_LINK,
+    customModels: process.env.CUSTOM_MODELS ?? "",
+    defaultModel: process.env.DEFAULT_MODEL ?? DEFAULT_MODELS[0],
+    googleApiKey: process.env.GOOGLE_API_KEY,
+    googleUrl: process.env.GOOGLE_URL,
+    gtmId: process.env.GTM_ID,
 
-    isStability,
-    stabilityUrl: process.env.STABILITY_URL,
-    stabilityApiKey: getApiKey(process.env.STABILITY_API_KEY),
-
-    isAzure,
+    // azure
     azureUrl: process.env.AZURE_URL,
-    azureApiKey: getApiKey(process.env.AZURE_API_KEY),
+    azureApiKey: process.env.AZURE_API_KEY,
     azureApiVersion: process.env.AZURE_API_VERSION,
 
-    isGoogle,
-    googleApiKey: getApiKey(process.env.GOOGLE_API_KEY),
-    googleUrl: process.env.GOOGLE_URL,
-
-    isAnthropic,
-    anthropicApiKey: getApiKey(process.env.ANTHROPIC_API_KEY),
-    anthropicApiVersion: process.env.ANTHROPIC_API_VERSION,
+    // anthropic
     anthropicUrl: process.env.ANTHROPIC_URL,
+    anthropicApiKey: process.env.ANTHROPIC_API_KEY,
+    anthropicApiVersion: process.env.ANTHROPIC_API_VERSION,
 
-    isBaidu,
+    // stability
+    stabilityUrl: process.env.STABILITY_URL,
+    stabilityApiKey: process.env.STABILITY_API_KEY,
+
+    // baidu
     baiduUrl: process.env.BAIDU_URL,
-    baiduApiKey: getApiKey(process.env.BAIDU_API_KEY),
+    baiduApiKey: process.env.BAIDU_API_KEY,
     baiduSecretKey: process.env.BAIDU_SECRET_KEY,
 
-    isBytedance,
-    bytedanceApiKey: getApiKey(process.env.BYTEDANCE_API_KEY),
+    // bytedance
     bytedanceUrl: process.env.BYTEDANCE_URL,
+    bytedanceApiKey: process.env.BYTEDANCE_API_KEY,
 
-    isAlibaba,
+    // alibaba
     alibabaUrl: process.env.ALIBABA_URL,
-    alibabaApiKey: getApiKey(process.env.ALIBABA_API_KEY),
+    alibabaApiKey: process.env.ALIBABA_API_KEY,
 
-    isTencent,
+    // tencent
     tencentUrl: process.env.TENCENT_URL,
-    tencentSecretKey: getApiKey(process.env.TENCENT_SECRET_KEY),
+    tencentSecretKey: process.env.TENCENT_SECRET_KEY,
     tencentSecretId: process.env.TENCENT_SECRET_ID,
 
-    isMoonshot,
+    // moonshot
     moonshotUrl: process.env.MOONSHOT_URL,
-    moonshotApiKey: getApiKey(process.env.MOONSHOT_API_KEY),
+    moonshotApiKey: process.env.MOONSHOT_API_KEY,
 
-    isIflytek,
+    // iflytek
     iflytekUrl: process.env.IFLYTEK_URL,
     iflytekApiKey: process.env.IFLYTEK_API_KEY,
     iflytekApiSecret: process.env.IFLYTEK_API_SECRET,
 
-    cloudflareAccountId: process.env.CLOUDFLARE_ACCOUNT_ID,
-    cloudflareKVNamespaceId: process.env.CLOUDFLARE_KV_NAMESPACE_ID,
-    cloudflareKVApiKey: getApiKey(process.env.CLOUDFLARE_KV_API_KEY),
-    cloudflareKVTTL: process.env.CLOUDFLARE_KV_TTL,
-
-    gtmId: process.env.GTM_ID,
-    gaId: process.env.GA_ID || DEFAULT_GA_ID,
-
-    needCode: ACCESS_CODES.size > 0,
-    code: process.env.CODE,
-    codes: ACCESS_CODES,
-
-    proxyUrl: process.env.PROXY_URL,
-    isVercel: !!process.env.VERCEL,
-
-    hideUserApiKey: !!process.env.HIDE_USER_API_KEY,
-    disableGPT4,
-    hideBalanceQuery: !process.env.ENABLE_BALANCE_QUERY,
-    disableFastLink: !!process.env.DISABLE_FAST_LINK,
-    customModels,
-    defaultModel,
-    allowedWebDavEndpoints,
+    publicModels,
   };
 };
